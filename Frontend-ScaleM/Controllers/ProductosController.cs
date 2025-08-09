0acd0a06bf757291b914b20d05f98c4d9dcd176e
DavidL <victordavidromerolebron75@gmail.com>
2025-08-08 23:15:43 -0400

    Api

---
+++ Frontend-ScaleM/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Frontend_ScaleM.Services;
+
+namespace Frontend_ScaleM.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly IProductosService _service;
+
+        public ProductosController(IProductosService service)
+        {
+            _service = service;
+        }
+
+        // GET: /Productos/JsonLines
+        public async Task<IActionResult> JsonLines()
+        {
+            var lineas = await _service.ObtenerLineasAsync();
+            return Json(lineas);
+        }
+    }
+}
